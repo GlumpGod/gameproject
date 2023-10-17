cff14d31b0bdb6553f010b25099c65120db3470b
Glumpy <100801667+GlumpGod@users.noreply.github.com>
2023-10-17 18:15:38 -0500

    fixed redundancy & added results of round

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-// variables for choices made by computer and player
+// variables
 
 let computerPlay;
 let playerChoice;
+let result;
 
 // function to get choice and store it
 
 function getCPUChoice() {
     const computerSelection = ["rock" , "paper" , "scissors"];
     const randomCPU = computerSelection[Math.floor(Math.random() * computerSelection.length)];
-    return randomCPU
+    return randomCPU.toLowerCase()
 };
 
 // function to get player input & const to prompt / store input
 
 function getPlayerChoice() {
     const userInput = prompt('Rock, Paper, or Scissors? ');
+    return userInput.toLowerCase()
+};
+
+// code for win condition
+
+function results() {
+    
+    if (playerChoice === 'rock' && computerPlay === 'scissors' || playerChoice === 'scissors' && computerPlay === 'paper' || playerChoice === 'paper' && computerPlay === 'rock') 
+            result = 'you win'
+        
+            else if (playerChoice === 'scissors' && computerPlay === 'rock' || playerChoice === 'paper' && computerPlay === 'scissors' || playerChoice === 'rock' && computerPlay === 'paper') 
+                result = 'you lose'
+            
+                else
+                    result = 'draw';
+                    
+                        return result;
 };
 
 // function to play a round of rock paper scissors
 
 function playRound() {
-    computerPlay = getCPUChoice().toLowerCase();
-    playerChoice = getPlayerChoice().toLowerCase();
+    computerPlay = getCPUChoice();
+    playerChoice = getPlayerChoice();
+    result = results();
+    console.log(computerPlay);
+    console.log(playerChoice);
+    console.log(result);
 };
 
-// save for later document.getElementById('results').innerHTML = "The winner is " + playerChoice + computerPlay ;
+
+// rock > scissors , scissors > paper , paper > rock
+
+// save for later document.getElementById('results').innerHTML = "The winner is " + playerChoice + computerPlay 
